fa17693a5068e417f84c63d93ea1f05e291d8949
sbranda <brandaster@gmail.com>
2026-09-12 22:46:10 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
 // Recordá bumpear esta versión en cada deploy
-const CACHE_NAME = 'mandalas-v42';
+const CACHE_NAME = 'mandalas-v43';
 const APP_SHELL = [
   './index.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  './splash.jpg'
 ];
 
 self.addEventListener('install', function(event){
